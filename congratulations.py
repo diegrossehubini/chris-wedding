bd10a83fd8f1ecdec7e71a4c1d0dfc98ce021b3a
diegrossehubini <126405057+diegrossehubini@users.noreply.github.com>
2023-02-26 16:35:33 +0100

    added congratulations

---
+++ congratulations.py
@@ -0,0 +1,17 @@
+groom = "Chris"
+bride = "Annika"
+congratulants = ["Judith", "Kamal", "Clemens", "Daniel", "Anna-Lena", "Simeon", "Joy", "Sebastian", "Selina", "Peter", "Léa", "Fabienne", "Jake", "Markus", "Steffi", "Elli", "Ellen", "Tina", "Alison", "Philipp", "Christine", "Nico", "Alexander", "Katharina", "Maximilian", "Carsten S.", "Merlin🐶"]
+
+def best_wishes(groom, bride, congratulant):
+    message = (
+    """
+    Lieber {}, liebe {},
+    zu eurer Hochzeit wünschen wir euch alles erdenklich Gute!
+    Wir freuen uns mit euch und hoffen, ihr hattet einen wunderschönen Tag.
+    Euer Myxo {}
+    """.format(groom, bride, congratulant))
+    
+    return message
+
+for congratulant in congratulants:
+    print(best_wishes(groom, bride, congratulant))
